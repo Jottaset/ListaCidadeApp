4785b7db936eaaa03f21a26bd73bda5060ef241a
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-18 20:43:21 -0400

    first commit

---
+++ ListaCidadeApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ListaCidadeApp.Model;
+
+namespace ListaCidadeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Cidade> listacidade = new List<Cidade>();
+        List<string> stringlista = new List<string>();
+
+        void GravarDados(object sender, System.EventArgs e)
+        {
+            Cidade cidade = new Cidade();
+
+            cidade.nome = label_cidade.Text;
+            cidade.uf =   label_estado.Text;
+
+            listacidade.Add(cidade);
+            stringlista.Add(cidade.nome + " " + cidade.uf);
+        }
+
+        void ChecarDados(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MyPageLista(stringlista));
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            }         
+    }
+}
---
+++ ListaCidadeApp/MyPageLista.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ListaCidadeApp.Model;
+
+namespace ListaCidadeApp
+{
+    public partial class MyPageLista : ContentPage
+    {
+
+        public MyPageLista(List<string> listaNova)
+        {
+            InitializeComponent();
+            ListaCidade.ItemsSource = listaNova;
+        }
+    }
+}
